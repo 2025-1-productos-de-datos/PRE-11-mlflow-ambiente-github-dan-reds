1fd1f209c53157582a7b6dec189b0977a9ddadd3
dan-reds <drojash@unal.edu.co>
2025-05-24 12:27:07 -0500

    Intento2

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -13,8 +13,7 @@ def test_01():
     try:
         subprocess.run(
             [
-                "source",
-                "run.sh",
+                "mlflow run https://github.com/dan-reds/mlflow-wine-quality.git -e knn -P n_neighbors=5"
             ],
             check=True,
         )
